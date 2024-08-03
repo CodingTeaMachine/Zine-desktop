4ff835f5c9b0c376c295c8d130ffde5dfe80419a
Dekany Mark <codingteamachine@gmail.com>
2024-08-03 09:07:11 +0200

    [v_0.2] --chore: make the dragging of comic books look good

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./**/*.{razor,cshtml,html}"],
+	content: ["./**/*.{razor,cshtml,html,css}"],
 	theme: {
 		extend: {
 			height: {
 				'group-card': '150px'
 			},
-			color: {
+			colors: {
 				primary: '#776be7' // mud-theme-primary
 			}
 		},
