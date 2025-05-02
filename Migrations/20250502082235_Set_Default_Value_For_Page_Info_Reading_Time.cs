3df05a38e13a1c23a2733b42cf0c1557ee74841f
Dekany Mark <codingteamachine@gmail.com>
2025-05-02 12:26:56 +0200

    --fix: reading time default value

---
+++ Migrations/20250502082235_Set_Default_Value_For_Page_Info_Reading_Time.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zine.Migrations
+{
+    /// <inheritdoc />
+    public partial class Set_Default_Value_For_Page_Info_Reading_Time : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "TimeSpentReadingPage",
+                table: "ComicBookPageInformation",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "TimeSpentReadingPage",
+                table: "ComicBookPageInformation",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "TEXT",
+                oldDefaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
